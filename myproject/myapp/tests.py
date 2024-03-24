6051bbbb1343f23463e879f9c5db7312e4821604
Davide Presti <94308234+davevad93@users.noreply.github.com>
2024-03-24 15:17:42 +0100

    Add app's tests

--- myproject/myapp/tests.py
+++ myproject/myapp/tests.py
@@ -1,3 +1,41 @@
 from django.test import TestCase
+from .models import Region, Country
 
-# Create your tests here.
+class RegionModelTestCase(TestCase):
+    def setUp(self):
+        self.region = Region.objects.create(name='Europe')
+
+    def test_region_str_representation(self):
+        self.assertEqual(str(self.region), 'Europe')
+
+class CountryModelTestCase(TestCase):
+    def setUp(self):
+        self.region = Region.objects.create(name='Europe')
+        self.country = Country.objects.create(
+            name='Portugal',
+            capital='Lisbon',
+            population=10229907,
+            region=self.region,
+            subregion='Southern Europe',
+            currency='Euro',
+            language='Portuguese',
+            borders='ESP',
+            area=92090.0,
+            timezone='UTC-01:00',
+            top_level_domain='.pt'
+        )
+
+    def test_country_str_representation(self):
+        self.assertEqual(str(self.country), 'Portugal')
+        self.assertEqual(self.country.name, 'Portugal')
+        self.assertEqual(self.country.capital, 'Lisbon')
+        self.assertEqual(self.country.population, 10229907)
+        self.assertEqual(self.country.region, self.region)
+        self.assertEqual(self.country.subregion, 'Southern Europe')
+        self.assertEqual(self.country.currency, 'Euro')
+        self.assertEqual(self.country.language, 'Portuguese')
+        self.assertEqual(self.country.borders, 'ESP')
+        self.assertEqual(self.country.area, 92090.0)
+        self.assertEqual(self.country.timezone, 'UTC-01:00')  
+        self.assertEqual(self.country.top_level_domain, '.pt')
+        
