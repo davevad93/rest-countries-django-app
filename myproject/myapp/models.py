81902225de0b76bc231dfde51926540b06d4adaa
Davide Presti <94308234+davevad93@users.noreply.github.com>
2024-03-24 13:58:50 +0100

    Add app's models

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Region(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+    capital = models.CharField(max_length=100)
+    population = models.IntegerField()
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+    subregion = models.CharField(max_length=100)
+    currency = models.CharField(max_length=100)
+    language = models.CharField(max_length=100)
+    borders = models.CharField(max_length=100)
+    area = models.FloatField()
+    timezone = models.CharField(max_length=100)
+    top_level_domain = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name  
