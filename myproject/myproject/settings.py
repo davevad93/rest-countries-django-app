e50c91945d00286f560f8c7666d03a3583ce4025
Davide Presti <94308234+davevad93@users.noreply.github.com>
2024-04-14 18:29:03 +0200

    Update settings.py

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +28,7 @@ STATICFILES_DIRS = [
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'uvo$_t-ih90962@kn5d1$fs=d1)0t2uk%o^zbxb5fe^*n#&3!n'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
