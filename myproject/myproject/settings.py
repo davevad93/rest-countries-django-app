840d582ecb24a0d5bd3abde3bfa227ed3b240e96
Davide Presti <94308234+davevad93@users.noreply.github.com>
2024-11-29 18:45:53 +0100

    Fix databases & add build_files.sh

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -92,10 +92,10 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
